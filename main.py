9ebef980f9783598d2800bc3f394a3a7ca0d7de9
Jakerya Patton <jakeryapatton@Jakeryas-Air.lan>
2023-11-03 22:00:22 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import math
+
+# Collecting radius input from the user
+radius = float(input("Enter the radius of the circle: "))
+
+# Calculating the area of the circle
+area = math.pi * (radius ** 2)
+
+# Printing the result
+print(f"The area of the circle is: {area}")
